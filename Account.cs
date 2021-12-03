c0a2caee960744cdc13ba12bf62f4c5a3eb5d146
Zakaria.LABIED <Zakaria.LABIED@SA-SI-BAIDA>
2021-12-03 12:25:47 +0100

    Ajoutez des fichiers projet.

---
+++ Account.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bank_Account
+{
+    class Account
+    {
+        public string name { get; set; }
+        private decimal balance;
+        private IList<Operations> opertaions = new List<Operations>();
+        public Account(string acountName, decimal balance)
+        {
+
+            name = acountName;
+            this.balance = balance;
+
+        }
+        public decimal Balance
+        {
+            get
+            {
+                return balance;
+            }
+            set
+            {
+                if (value >= 0.0m)
+                { balance = value; }
+            }
+        }
+        public void Deposit(decimal depositAmount, string date)
+        {
+            if (depositAmount > 0.0m)
+            {
+                Balance += depositAmount;
+                this.opertaions.Add(new Operations(depositAmount, date, "Deposit"));
+            }
+        }
+        public void Withdrawal(decimal withdrawAmount, string date)
+        {
+            if (Balance > 0 && Balance - withdrawAmount >= 0.0m)
+            {
+                Balance -= withdrawAmount;
+                this.opertaions.Add(new Operations(withdrawAmount, date, "Withdraw"));
+            }
+        }
+        public void printStatemnt()
+        {
+            Console.WriteLine($"Client :{name}");
+            Console.WriteLine("Amout || Date || Type ");
+
+            foreach (var operation in opertaions)
+            {
+                Console.WriteLine("{0} || {1} || {2}", operation.amount, operation.date, operation.type);
+            }
+        }
+    }
+        struct Operations
+        {
+            public decimal amount;
+            public string date;
+            public string type;
+            public Operations(decimal amount, string date, string type)
+            {
+                this.amount = amount;
+                this.date = date;
+                this.type = type;
+            }
+        }
+
+    
+}
---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Bank_Account
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account account1 = new Account("persone1",200.0m);
+            Account account2 = new Account("persone2", 200.0m);
+
+            Operations op1 = new Operations(account1.Balance, "01/12/2021", "deposit");
+            account1.Deposit(op1.amount, op1.date);
+            Console.WriteLine("-------------------");
+            account1.printStatemnt();
+            Console.WriteLine("-------------------");
+
+        }
+    }
+}
