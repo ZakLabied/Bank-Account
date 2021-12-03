435e76e2d6b8be802ed3d8cd71b45eb594b15393
ZakLabied <65694164+ZakLabied@users.noreply.github.com>
2021-12-03 12:38:06 +0100

    ZL

--- Program.cs
+++ Program.cs
@@ -10,10 +10,13 @@ namespace Bank_Account
             Account account2 = new Account("persone2", 200.0m);
 
             Operations op1 = new Operations(account1.Balance, "01/12/2021", "deposit");
+            Operations op2 = new Operations(300.0m, "02/12/2021", "withdraw");
             account1.Deposit(op1.amount, op1.date);
+            account1.Withdrawal(op2.amount, op2.date);
             Console.WriteLine("-------------------");
             account1.printStatemnt();
             Console.WriteLine("-------------------");
+            Console.WriteLine($"Your balance is : {account1.Balance}$");
 
         }
     }
